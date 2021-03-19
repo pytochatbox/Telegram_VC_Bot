3088bf1ab3d0fee2bc60059ec8e3cbb3f19f4a5e
pytochatbox <77884261+pytochatbox@users.noreply.github.com>
2021-03-19 15:27:48 +0530

    Update and rename sample_config.py to config.py
    Update config

---
+++ config.py
@@ -0,0 +1,8 @@
+api_id = 2048409
+api_hash = "649e419e5fa739da02b649780d492c1b"
+sudo_chat_id = -1001235672935
+owner_id = 1217374522
+
+
+# Arq Api, don't make changes here
+ARQ_API = "https://thearq.tech"
